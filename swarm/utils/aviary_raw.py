f5086a291c14e49e26a0a33b4f548546c50c8aac
miguel <miguel@computebnb.io>
2025-06-23 10:29:51 +0200

    Updated readme.md

--- swarm/utils/aviary_raw.py
+++ swarm/utils/aviary_raw.py
@@ -1,10 +1,9 @@
 # aviary_raw.py ----------------------------------------------------------
 from gym_pybullet_drones.envs.HoverAviary import HoverAviary
-from gym_pybullet_drones.utils.enums import ActionType
 import numpy as np
 
 class HoverAviaryRawRPM(HoverAviary):
-    """ActionType.RPM but the value *is* the RPM (0 … MAX_RPM)."""
+    """Override HoverAviary so that it can receive raw RPMs"""
     def _preprocessAction(self, action):
         # action shape = (num_drones, 4)
         return np.clip(action, 0.0, self.MAX_RPM)
