edf961d5eae9eb5f074a3feb4bb564b615af9453
S3333F <1alitsaaf@gmail.com>
2025-09-01 10:26:50 +0300

    goal distance range 3,30 -> 10,30

--- swarm/constants.py
+++ swarm/constants.py
@@ -59,7 +59,7 @@ SAFE_Z = 3                              # Default cruise altitude (meters)
 GOAL_TOL = LANDING_PLATFORM_RADIUS * 0.8 * 1.06  # TAO badge radius for precision landing (0.5088m)
 SPEED_LIMIT = 3.0                       # Maximum drone velocity limit (m/s)
 # Goal generation ranges
-R_MIN, R_MAX = 3, 30                    # Radial goal distance range (meters)
+R_MIN, R_MAX = 10, 30                    # Radial goal distance range (meters)
 H_MIN, H_MAX = 1, 10                    # Height variation range for goals (meters)
 # Environment building limits
 SAFE_ZONE_RADIUS = 2.0                  # Minimum clearance around obstacles (meters)
