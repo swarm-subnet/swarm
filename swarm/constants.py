65a8e34d73e90d1bc868c4f9dd38f77843bf1a13
S3333F <1alitsaaf@gmail.com>
2025-10-14 17:33:26 +0300

    Number of runs to track victory average 10 -> 50

--- swarm/constants.py
+++ swarm/constants.py
@@ -94,7 +94,7 @@ UID_ZERO = 0                            # Special UID for burning emissions
 
 # Reward distribution mechanism
 WINNER_TAKE_ALL = True                  # Enable winner-take-all rewards (winner gets all available emissions)
-N_RUNS_HISTORY = 10                     # Number of runs to track for victory average
+N_RUNS_HISTORY = 50                     # Number of runs to track for victory average
 
 # =============================================================================
 # CHALLENGE TYPE DISTRIBUTION
