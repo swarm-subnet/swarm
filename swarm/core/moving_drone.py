6f433887212d199afb2caaa8f5986d1b09648fde
S3333F <1alitsaaf@gmail.com>
2026-03-12 08:37:04 +0100

    hide default checkerboard plane for forest maps

--- swarm/core/moving_drone.py
+++ swarm/core/moving_drone.py
@@ -850,6 +850,13 @@ class MovingDroneAviary(BaseRLAviary):
             physicsClientId=cli,
         )
 
+        if getattr(self.task, "challenge_type", 0) == 6:
+            plane_id = getattr(self, "PLANE_ID", None)
+            if plane_id is not None:
+                p.changeVisualShape(
+                    plane_id, -1, rgbaColor=[0, 0, 0, 0], physicsClientId=cli,
+                )
+
         self._build_cull_targets()
 
     # -------- reward ----------------------------------------------------- #
